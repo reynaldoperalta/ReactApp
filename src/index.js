1591902e82019acbc839ce7eec4f22192cd1b04b
reynaldoperalta <reynaldopema14@gmail.com>
2021-03-26 19:21:24 -0500

    Mi segundo commit - eliminacion de archivos

--- src/index.js
+++ src/index.js
@@ -1,43 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Content = ... {
-  return (
-    <div>
-      <Part .../>
-      <Part .../>
-      <Part .../>
-    </div>
-  )
-}
-
-
-
-const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises = 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises = 7
-    },
-    {
-      name: 'State of a component',
-      exercises = 14
-    }
-  ]
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content ... />
-      <Total ... />
-    </div>
-  )
-}
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
